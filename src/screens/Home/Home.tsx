1dd4ea12c09457767d68e92646f0de1fc0d580d8
Guilherme Augusto Borges Pinheiro Junior <044748@campus1.uniaraxa.edu.br>
2024-03-07 20:07:11 -0300

    first commit

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,16 @@
+
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+
+const Home = () => {
+
+return (
+    <View>
+        <Text>Estou na Home</Text>        
+    </View>
+
+);
+
+};
+
+export default Home;
